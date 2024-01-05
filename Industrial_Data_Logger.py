b49462d5262145632698667752c52e791bc152e5
Anand chaurasia <151776571+AnandCh20@users.noreply.github.com>
2024-01-05 17:48:15 +0530

    Industrial_Data_Logger.py

--- Industrial_Data_Logger.py
+++ Industrial_Data_Logger.py
@@ -1,22 +1,30 @@
 # data_logger.py
+
 import time
 import random
+
 from datetime import datetime
-import sqlite3
+import mysql.connector
 
 
-def log_data_to_database() -> object:
-    # Connect to the SQLite database
-    conn = sqlite3.connect('industrial_data.db')
+def log_data_to_database():
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(
+        host="192.168.217.105",
+        user="root",
+        password="123456",
+        database="industrial_data"
+    )
     cursor = conn.cursor()
 
     while True:
         # Simulate data from sensors
         sensor_data = random.uniform(20, 100)
+        print(sensor_data)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         # Insert data into the database
-        cursor.execute("INSERT INTO sensor_data (timestamp, value) VALUES (?, ?)", (timestamp, sensor_data))
+        cursor.execute("INSERT INTO sensor_data (timestamp, value) VALUES (%s, %s)", (timestamp, sensor_data))
         conn.commit()
 
         time.sleep(5)  # Log data every 5 seconds
